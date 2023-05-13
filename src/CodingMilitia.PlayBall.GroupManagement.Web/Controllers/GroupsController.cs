2d87a444550456bbc2ad6593497ce84b452ca171
John Doe <johndoe@example.com>
2023-05-13 12:23:30 +0800

    系统日志使用说明

--- src/CodingMilitia.PlayBall.GroupManagement.Web/Controllers/GroupsController.cs
+++ src/CodingMilitia.PlayBall.GroupManagement.Web/Controllers/GroupsController.cs
@@ -4,6 +4,7 @@ using CodingMilitia.PlayBall.GroupManagement.Business.Services;
 using CodingMilitia.PlayBall.GroupManagement.Web.Mappings;
 using Microsoft.Extensions.Options;
 using CodingMilitia.PlayBall.GroupManagement.Web.Demo;
+using CodingMilitia.PlayBall.GroupManagement.Web.LoggingTool;
 
 namespace CodingMilitia.PlayBall.GroupManagement.Web.Controllers
 {
@@ -14,19 +15,32 @@ namespace CodingMilitia.PlayBall.GroupManagement.Web.Controllers
 		private readonly IGroupsService _groupsService;
 		private readonly SomeRootConfiguration _config;
 		private readonly SomeSecretsConfiguration _scecret;
+		private readonly ILogger<GroupsController> _logger;
 
-		public GroupsController(IGroupsService groupsService, IOptions<SomeRootConfiguration> config, IOptions<SomeSecretsConfiguration> scecret)
+		public GroupsController(IGroupsService groupsService, IOptions<SomeRootConfiguration> config, IOptions<SomeSecretsConfiguration> scecret, ILogger<GroupsController> logger)
 		{
 			_groupsService = groupsService;
 			// _config不仅能获取到 appsettings.json 中 SomeRoot 节点中的2个值，还能获取到运行时的参数携带的值 CmdLineKey	456789
 			_config = config.Value;
 			_scecret = scecret.Value;
+			_logger = logger;
 		}
 
 		// GET: /groups/index
 		public IActionResult Index()
 		{
 			//return Content("Hello world!");
+			_logger.LogCritical("***************************Home Page Critical 严重级别***************************");
+			_logger.LogWarning("***************************Home Page Warning 警告级别***************************");
+			_logger.LogDebug("***************************Home Page Debug 调试级别***************************");
+			_logger.LogError("***************************Home Page Error 错误级别***************************");
+			_logger.LogTrace("***************************Home Page Trace 追踪级别***************************");
+			// 注意：{}中的实际显示的值由后面参数位置决定，而不是{}中的名称（意味着可以任意取名）；与插值表达式有点不同；只与位置有关与名称无关
+			_logger.LogInformation(MyLogEvents.GetItem,
+						  " [{time}] [{id}]-{hhhh} ****Home Page Information 信息级别**********",
+						  DateTime.Now.ToString("O"),
+						  MyLogEvents.GetItem,
+						  nameof(Index));
 			return View(_groupsService.GetAll().ToViewModel());
 		}
 
